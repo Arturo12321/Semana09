775e78d86d6a1709af0a6d17694b914888d1f250
Alumno <Alumno@LAB1504-03>
2023-10-24 21:20:43 -0500

    Agregar archivos de proyecto.

---
+++ MVC/Controllers/ProductController.cs
@@ -0,0 +1,107 @@
+﻿using Business;
+using Entity1;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            BProduct bproduct = new BProduct();
+
+            List<Product> products = bproduct.GetAllProducts();
+
+
+            List<ProductModel> productsModel = products.Select(x => new ProductModel
+                {
+                    ProductId = x.ProductId,
+                    Name = x.Name,
+                    Price = x.Price,
+                    Stock = x.Stock,
+                }).ToList();
+            return View(productsModel);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProductModel model)
+        {
+            try
+            {
+                BProduct bproduct = new BProduct();
+                 
+                Product newProduct = new Product
+                {
+                    Name = model.Name,
+                    Price = model.Price,
+                    Stock = model.Stock
+                };
+                bproduct.CreateProduct(newProduct);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC/Models/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
